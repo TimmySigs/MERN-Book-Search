5f8ef1c018b6a99fb15dd3813ce86b84eae51202
TimmySigs <108436098+TimmySigs@users.noreply.github.com>
2022-12-20 02:11:13 -0600

    refactoring mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,32 +1,51 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
       user {
-        _id
+        id
         username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          title
+          image
+          link
+        }
       }
     }
   }
 `;
-
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
       token
       user {
-        _id
+        id
         username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          title
+          image
+          link
+        }
       }
     }
   }
 `;
-
 export const SAVE_BOOK = gql`
-  mutation saveBook($bookToSave: bookInput) {
-    saveBook(bookToSave: $bookToSave) {
+  mutation saveBook($input: BookInput) {
+    saveBook(input: $input) {
+      id
       username
       email
       bookCount
@@ -41,10 +60,10 @@ export const SAVE_BOOK = gql`
     }
   }
 `;
-
 export const REMOVE_BOOK = gql`
-  mutation removeBook($bookId: String!) {
+  mutation removeBook($bookId: ID!) {
     removeBook(bookId: $bookId) {
+      id
       username
       email
       bookCount
