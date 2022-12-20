efaa15819b7e6a7b61a3e959249246b2d2a34e1e
TimmySigs <108436098+TimmySigs@users.noreply.github.com>
2022-12-20 02:29:32 -0600

    changes in resolvers to try and fix login

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,20 +1,18 @@
-const { User, Book } = require('../models');
-
-const { AuthenticationError } = require('apollo-server-express');
-const { signToken } = require('../utils/auth');
+const { User } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
     Query: {
         me: async (parent, args, context) => {
             if (context.user) {
                 const userData = await User.findOne({ _id: context.user._id })
-                    .select('-__v -password')
-                    .populate('savedBooks');
+                    .select("-__v -password")
+                    .populate("savedBooks");
 
                 return userData;
             }
-
-            throw new AuthenticationError('Not logged in');
+            throw new AuthenticationError("You are not logged in");
         },
     },
     Mutation: {
@@ -22,50 +20,50 @@ const resolvers = {
             const user = await User.findOne({ email });
 
             if (!user) {
-                throw new AuthenticationError('Incorrect credentials');
+                throw new AuthenticationError("username/password is incorrect");
             }
 
             const correctPw = await user.isCorrectPassword(password);
 
             if (!correctPw) {
-                throw new AuthenticationError('Incorrect credentials');
+                throw new AuthenticationError("username/password is incorrect");
             }
-
-            const token = signToken(user);
-            return { token, user };
-        },
-
-        addUser: async (parent, args) => {
-            const user = await User.create(args);
             const token = signToken(user);
 
             return { token, user };
         },
+        // addUser(username: String!, email: String!, password: String!): Auth
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
 
+            return { token, user };
+        },
+        // saveBook(input: BookInput): User
+        saveBook: async (parent, { input }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: input } },
+                    { new: true }
+                ).populate("savedBooks");
+
+                return updatedUser;
+            }
+            throw new AuthenticationError("You must be logged in!");
+        },
+        // removeBook(bookId: ID!): User
         removeBook: async (parent, { bookId }, context) => {
             if (context.user) {
-                const updatedBooks = await User.findOneAndUpdate(
+                const updatedUser = await User.findOneAndUpdate(
                     { _id: context.user._id },
-                    { $pull: { savedBooks: { bookId } } },
+                    { $pull: { savedBooks: { bookId: bookId } } },
                     { new: true }
-                );
+                ).populate("savedBooks");
 
-                return updatedBooks;
+                return updatedUser;
             }
-        },
-
-        saveBook: async (parent, { bookToSave }, context) => {
-            if (context.user) {
-                const updatedBooks = await User.findOneAndUpdate(
-                    { _id: context.user._id },
-                    { $addToSet: { savedBooks: bookToSave } },
-                    { new: true }
-                ).populate('savedBooks');
-
-                return updatedBooks;
-            }
-
-            throw new AuthenticationError('You need to be logged in!');
+            throw new AuthenticationError("You must be logged in!");
         },
     },
 };
