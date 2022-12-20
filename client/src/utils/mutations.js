db1ed87d15ecdf25f3fadc255bba4bf6fda7d04f
TimmySigs <108436098+TimmySigs@users.noreply.github.com>
2022-12-20 02:48:56 -0600

    reverting mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,51 +1,32 @@
-import { gql } from "@apollo/client";
+import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
       user {
-        id
+        _id
         username
-        email
-        bookCount
-        savedBooks {
-          bookId
-          authors
-          description
-          title
-          image
-          link
-        }
       }
     }
   }
 `;
+
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
       token
       user {
-        id
+        _id
         username
-        email
-        bookCount
-        savedBooks {
-          bookId
-          authors
-          description
-          title
-          image
-          link
-        }
       }
     }
   }
 `;
+
 export const SAVE_BOOK = gql`
-  mutation saveBook($input: BookInput) {
-    saveBook(input: $input) {
-      id
+  mutation saveBook($bookToSave: bookInput) {
+    saveBook(bookToSave: $bookToSave) {
       username
       email
       bookCount
@@ -60,10 +41,10 @@ export const SAVE_BOOK = gql`
     }
   }
 `;
+
 export const REMOVE_BOOK = gql`
-  mutation removeBook($bookId: ID!) {
+  mutation removeBook($bookId: String!) {
     removeBook(bookId: $bookId) {
-      id
       username
       email
       bookCount
